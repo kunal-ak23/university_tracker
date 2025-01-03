4867db9a0c238c272576efc2bee0482f6076e012
kunal sharma <kunalsharma.ak23@gmail.com>
2025-01-03 21:12:32 +0530

    remove frontend folder.

--- frontend/src/auth.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
-
-// @ts-ignore
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  ...authConfig,
-})
